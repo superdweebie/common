f6db9d54ee1f93ac7996844114169c5eea7903ac
Tim Roediger <superdweebie@gmail.com>
2012-06-24 20:02:44 +1000

    Added freeze interfaces

---
+++ lib/SdsCommon/Freeze/ThawedOnInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link       http://superdweebie.com
+ * @package    Sds
+ * @license    MIT
+ */
+namespace SdsCommon\Freeze;
+
+/**
+ * Interface to add thawed on stamp to object
+ *
+ * @since   1.0
+ * @author  Tim Roediger <superdweebie@gmail.com>
+ */
+interface ThawedOnInterface{
+
+    /**
+     * The time of thaw
+     *
+     * @param timestamp $timestamp
+     */
+    public function setThawedOn($timestamp);
+
+    /**
+     * @return timestamp
+     */
+    public function getThawedOn();
+}
