a7a26441a4160bda42b679b13282867e58b9380d
Tim Roediger <tim.roediger@ausaero.com.au>
2012-07-13 10:42:43 +1000

    Fix error in ValidatorInterface

--- lib/Sds/common/Validator/ValidatorInterface.php
+++ lib/Sds/common/Validator/ValidatorInterface.php
@@ -1,18 +1,16 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Validator
+ * @link       http://superdweebie.com
+ * @package    Sds
+ * @license    MIT
  */
-
-namespace Zend\Validator;
+namespace Sds\Common\Validator;
 
 /**
- * @category   Zend
- * @package    Zend_Validate
+ * Inspired by Zend\Validator\ValidatorInterface
+ *
+ * @since   1.0
+ * @author  Tim Roediger <superdweebie@gmail.com>
  */
 interface ValidatorInterface
 {
