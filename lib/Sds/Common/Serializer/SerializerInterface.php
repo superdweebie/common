e2f90455eac2a7eda5d422e5379811209f937b61
Tim Roediger <tim.roediger@ausaero.com.au>
2012-08-06 16:38:23 +1000

    Added serializer interface

---
+++ lib/Sds/Common/Serializer/SerializerInterface.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package    Sds
+ * @license    MIT
+ */
+namespace Sds\Common\Serializer;
+
+/**
+ *
+ * @since   1.0
+ * @version $Revision$
+ * @author  Tim Roediger <superdweebie@gmail.com>
+ */
+interface SerializerInterface
+{
+
+    /**
+     *
+     * @param object $object
+     * @return array
+     */
+    public function toArray($object);
+
+    /**
+     *
+     * @param array $object
+     * @return string
+     */
+    public function toJson($object);
+
+    /**
+     *
+     * @param array $data
+     * @param string $classNameKey
+     * @param string $className
+     * @return object
+     */
+    public function fromArray(array $data, $classNameKey = '_className', $className = null);
+
+    /**
+     *
+     * @param string $data
+     * @param string $classNameKey
+     * @param string $className
+     */
+    public function fromJson($data, $classNameKey = '_className', $className = null);
+}
+
