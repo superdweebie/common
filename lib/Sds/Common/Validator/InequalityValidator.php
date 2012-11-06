52705315eccf0e5a09bff448b9c92a7a66cb8516
Tim Roediger <tim.roediger@ausaero.com.au>
2012-11-06 16:03:00 +1100

    Minor fix

--- lib/Sds/Common/Validator/InequalityValidator.php
+++ lib/Sds/Common/Validator/InequalityValidator.php
@@ -15,9 +15,9 @@ class InequalityValidator extends AbstractValidator
 {
 
     protected $operator;
-    
+
     protected $compareValue;
-    
+
     public function __construct($operator, $compareValue) {
         $this->operator = (string) $operator;
         $this->compareValue = (float) $compareValue;
@@ -33,35 +33,35 @@ class InequalityValidator extends AbstractValidator
             case '>':
                 if ( ! ($this->compareValue > $value)){
                     $result = false;
-                    $this->messages[] = 'Value must be less than ' . $this->compareValue;
-                }                
+                    $this->messages[] = 'Must be less than ' . $this->compareValue;
+                }
                 break;
             case '>=':
                 if ( ! ($this->compareValue >= $value)){
                     $result = false;
-                    $this->messages[] = 'Value must be less than or equal to ' . $this->compareValue;
-                }                  
+                    $this->messages[] = 'Must be less than or equal to ' . $this->compareValue;
+                }
                 break;
             case '<':
                 if ( ! ($this->compareValue < $value)){
                     $result = false;
-                    $this->messages[] = 'Value must be greater than ' . $this->compareValue;
-                }                  
+                    $this->messages[] = 'Must be greater than ' . $this->compareValue;
+                }
                 break;
             case '<=':
                 if ( ! ($this->compareValue <= $value)){
                     $result = false;
-                    $this->messages[] = 'Value must be greater than or equal to ' . $this->compareValue;
-                }                  
+                    $this->messages[] = 'Must be greater than or equal to ' . $this->compareValue;
+                }
                 break;
             case '!=';
                 if ( ! ($this->compareValue != $value)){
                     $result = false;
-                    $this->messages[] = 'Value must not be equal to ' . $this->compareValue;
-                }                  
+                    $this->messages[] = 'Must not be equal to ' . $this->compareValue;
+                }
                 break;
         }
-        
+
         return $result;
     }
 }
